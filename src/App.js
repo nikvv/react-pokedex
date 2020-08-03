e88cc4dde31f0aac31a928469c4502bfa17bcb25
NickoValentino <nicko.valentinoo@gmail.com>
2020-08-04 02:06:35 +0800

    -Add bulma cdn to index.html -Import and use components in App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import PokemonCard from './components/PokemonCard.js'
+import Hero from './components/Hero.js'
+export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      pokemons: [],
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  async componentDidMount() {
+   let pokemons = await (await fetch('https://pokeapi.co/api/v2/pokemon/?limit=151') ).json()
+      this.setState({
+        pokemons: pokemons.results
+      })
+  }
+  render() {
+    const {pokemons} = this.state
+    // console.log(pokemons);
+    if(!pokemons) {
+      return <h1>LOADING</h1>
+    }
+    else{
+    return (
+      <section className="section has-background-light">
+        <Hero />
+        <div className="container">
+        <div className="columns is-multiline">
+             {
+        pokemons.map(pokemon => (
+          <div key={pokemon.name}  className="column is-2">
+          <PokemonCard pokemon={pokemon} />
+          </div>
+
+        ))
+        }
+      </div>
+        </div>
+      </section>
+
+    )
+      }
+  }
 }
-
-export default App;
